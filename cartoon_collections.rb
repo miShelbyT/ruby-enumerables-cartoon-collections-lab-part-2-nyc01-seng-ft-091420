c5feec271bc8b99b3150ea5b08ea4ef953015157
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-29 20:22:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ end
 def find_valid_calls(planeteer_calls)
   # valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   binding.pry
-  planeteer_calls.find do (valid_calls)
+  planeteer_calls.find do { |string| string == valid_calls)
   end
 end
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above

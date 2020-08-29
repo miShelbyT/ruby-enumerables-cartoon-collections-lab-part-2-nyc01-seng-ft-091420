b40aed5be38734ecae249df5741233d891444a9c
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-29 20:24:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
   # Return the boolean result of this check
 
 def find_valid_calls(planeteer_calls)
-  # valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find { |string| string == valid_calls }
 end
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
